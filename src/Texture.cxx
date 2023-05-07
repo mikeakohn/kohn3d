60d6d13a6b14ec0bd07482107f21bb92cd222bf8
Michael Kohn <mike@mikekohn.net>
2023-05-07 15:49:14 -0500

    Adding code to convert between polar and cartesian coordinates.

--- src/Texture.cxx
+++ src/Texture.cxx
@@ -37,9 +37,38 @@ uint32_t Texture::get_pixel(double u, double v)
 
 void Texture::set_scale(int x0, int y0, int x1, int y1, int x2, int y2)
 {
-  float a = compute_angle(x0, y0, x1, y1, x2, y2);
+  sorted.x0 = x0;
+  sorted.y0 = y0;
+  sorted.x1 = x1;
+  sorted.y1 = y1;
+  sorted.x2 = x2;
+  sorted.y2 = y2;
 
-  printf("%f %f\n", a, 360 * (a / 6.28));
+  angle_xy = compute_angle(x0, y0, x1, y1, x2, y2);
+
+  angle_uv = compute_angle(
+    sorted.u0 * 1000, sorted.v0 * 1000,
+    sorted.u1 * 1000, sorted.v1 * 1000,
+    sorted.u2 * 1000, sorted.v2 * 1000);
+
+  angle_scale = angle_xy / angle_uv;
+
+  side_0_xy = compute_length(x1 - x0, y1 - y0);
+  side_1_xy = compute_length(x1 - x2, y1 - y2);
+
+  side_0_uv = compute_length(
+    (picture.get_width() * sorted.u1) - (picture.get_width() * sorted.u0),
+    (picture.get_height() * sorted.v1) - (picture.get_height() * sorted.v0));
+
+  side_1_uv = compute_length(
+    (picture.get_width() * sorted.u1) - (picture.get_width() * sorted.u2),
+    (picture.get_height() * sorted.v1) - (picture.get_height() * sorted.v2));
+
+  side_0_scale = side_0_xy / side_0_uv;
+  side_1_scale = side_1_xy / side_1_uv;
+
+  printf("%f %f\n", angle_xy, 360 * (angle_xy / 6.28));
+  printf("%f %f\n", angle_uv, 360 * (angle_uv / 6.28));
 }
 
 float Texture::compute_angle(int x0, int y0, int x1, int y1, int x2, int y2)
--- src/Texture.h
+++ src/Texture.h
@@ -40,6 +40,59 @@ private:
     return sqrt((a * a) + (b * b));
   }
 
+  void polar_to_cartesian(int &x, int &y, float a, int r)
+  {
+    // x = r * cos(a);
+    // y = r * sin(a);
+
+    x = r * cos(a);
+    y = r * sin(a);
+  }
+
+  void cartesian_to_polar(float &a, int &r, int x, int y)
+  {
+    // r = sqrt(x ^ 2 + y ^2);
+    // a = atan2(y / x);
+
+    r = sqrt((x * x) + (y * y));
+
+    if (x == 0)
+    {
+      if (y > 0) { a = 6.28 / 4; }
+      else if (y < 0) { a = (6.28 / 4) * 3; }
+      else { a = 0; }
+      return;
+    }
+
+    if (y == 0)
+    {
+      if (x > 0) { a = 0; }
+      else if (x < 0) { a = (6.28 / 2); }
+      else { a = 0; }
+      return;
+    }
+
+    if (x > 0 && y < 0)
+    {
+      // atan(-1 / 4) will come out negative.
+      a = 6.28 + atan(y / x);
+    }
+      else
+    if (x < 0 && y < 0)
+    {
+      a = 3.14 + atan(y / x);
+    }
+      else
+    if (x < 0 && y > 0)
+    {
+      a = 3.14 + atan(y / x);
+    }
+      else
+    {
+      a = atan(y / x);
+    }
+  }
+
   // (x1, y1) is the middle vertex where the angle is calculated.
   float compute_angle(int x0, int y0, int x1, int y1, int x2, int y2);
 
@@ -48,7 +101,21 @@ private:
     double u0, v0;
     double u1, v1;
     double u2, v2;
+
+    int x0, y0;
+    int x1, y1;
+    int x2, y2;
   } sorted;
+
+  float angle_xy;
+  float angle_uv;
+  int side_0_xy;
+  int side_0_uv;
+  int side_1_xy;
+  int side_1_uv;
+  float angle_scale;
+  float side_0_scale;
+  float side_1_scale;
 
   Picture picture;
 };
