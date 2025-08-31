1c5533bd930af761ea1622a35b5434e046f614e5
Michael Kohn <mike@mikekohn.net>
2025-08-31 16:00:14 -0500

    Save state before removing the rotation.

--- src/Angle.h
+++ src/Angle.h
@@ -75,16 +75,19 @@ public:
 
   int compute_length_at_1(double p, int &i, int &j)
   {
-    int x0, y0;
+    //double dp = p - coords_0.p;
 
+    int x0, y0;
     coords_0.to_xy(x0, y0);
 
 static int count = 0;
+#if 0
 printf("%.1f   (%d, %d)\n", (float)count / 10.0, x0, y0);
 printf("%.1f, %.1f\n",
   (float)x0 + (delta_x * ((float)count / 10.0)),
   (float)y0 + (delta_y * ((float)count / 10.0))
 );
+#endif
 
     int x = (float)x0 + (delta_x * ((float)count / 10.0));
     int y = (float)y0 + (delta_y * ((float)count / 10.0));
@@ -104,18 +107,19 @@ count = count + 1;
   {
     double dp = p - coords_0.p;
 
-    int x0, y0; //, x1, y1;
+    int x0, y0;
 
-    coords_0.to_xy(x0, y0);
-    //coords_1.to_xy(x1, y1);
+    coords_0.to_xy_centered(x0, y0);
 
+//printf("%.2f  %d\n", coords_0.p, coords_0.r);
 //printf("%.3f   (%d, %d)\n", dp /delta_p, x0, y0);
 
     int x = x0 + delta_x * (dp / delta_p);
     int y = y0 + delta_y * (dp / delta_p);
 
     PolarCoords coords = coords_0;
-    coords.from_xy(x, y);
+    coords.from_xy_centered(x, y);
+//coords.dump();
 
     return coords.r;
   }
